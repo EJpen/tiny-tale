9a5333cd4c87303e7cd2eb7539859cf69eec80c6
Ervin John Penales <theb8681@gmail.com>
2025-10-31 17:28:33 +0800

    Fix Next.js 16 Turbopack build errors - remove webpack config, add turbopack flags

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,8 @@
 module.exports = {
   experimental: {
     serverActions: {
-      allowedDevOrigins: [process.env.CORS_ORIGINS || "http://localhost:3000"],
+      allowedDevOrigins: [ process.env.CORS_ORIGINS || 'http://localhost:3000' ],
     },
   },
-  webpack: (config: any, { isServer }: { isServer: boolean }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), "_http_common"];
-    }
-    return config;
-  },
-};
+  turbopack: {},
+}
